17b44a5bd1849e9d978f8f4e469956111228b3b5
Debasmita Chatterjee <debasmitacjee@gmail.com>
2026-03-05 21:22:08 +0530

    Added Video Pipeline

---
+++ app/pipelines/video_pipeline.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+
+from .video_pipeline_folder.audio_extractor import extract_audio
+from .video_pipeline_folder.frame_extractor import extract_frames
+from audio_pipeline import run_audio_pipeline
+from image_pipeline import parse_document
+
+
+def video_parsing_pipeline(video_path):
+
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Video file not found: {video_path}")
+
+    audio_output = "temp_audio.wav"
+    frames_folder = "frames"
+
+    try:
+        audio_file = extract_audio(video_path, audio_output)
+    
+        frames = extract_frames(
+            video_path,
+            frames_folder,
+            hist_threshold=0.75,      
+            min_area_ratio=0.30,      # Only accept large changes
+            check_interval=25        # Check every 25th frame
+        )
+
+        
+        transcript_path = run_audio_pipeline(
+            audio_file,
+            enable_diarization=False
+        )
+
+        
+        parsed_doc = []
+        for frame_path in frames:
+            result = parse_document(frame_path)
+            parsed_doc.append({
+                 "frame": frame_path,
+                    "structured_output": result.get("structured_image_output", {}),
+                    "ocr_confidence": result.get("ocr_confidence", 0)
+            })
+        
+
+        return {
+            "transcript_file": transcript_path,
+            "frames_folder": frames_folder,
+            "total_slides": len(frames),
+            "parsed_documents": parsed_doc
+        }
+
+    
+    finally:
+        
+            if os.path.exists(audio_output):
+                os.remove(audio_output)
+            if os.path.exists(frames_folder):
+                shutil.rmtree(frames_folder)
+
---
+++ app/pipelines/video_pipeline_folder/frame_extractor.py
@@ -0,0 +1,93 @@
+import os
+import cv2
+import numpy as np
+
+
+def extract_frames(video_path,
+                   output_dir,
+                   hist_threshold=0.70,
+                   min_area_ratio=0.25,
+                   check_interval=20):
+
+    if not os.path.exists(video_path):
+        raise FileNotFoundError(f"Video file not found: {video_path}")
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    cap = cv2.VideoCapture(video_path)
+
+    prev_gray = None
+    prev_hist = None
+    frame_index = 0
+    saved_count = 0
+    frame_paths = []
+
+    total_pixels = None
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        
+        if frame_index % check_interval != 0:
+            frame_index += 1
+            continue
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+        if prev_gray is None:
+            prev_gray = gray
+            total_pixels = gray.shape[0] * gray.shape[1]
+
+            
+            prev_hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+            cv2.normalize(prev_hist, prev_hist)
+
+            frame_path = os.path.join(output_dir, f"frame_{saved_count:04d}.jpg")
+            cv2.imwrite(frame_path, frame)
+            frame_paths.append(frame_path)
+            saved_count += 1
+
+            frame_index += 1
+            continue
+
+        
+        curr_hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
+        cv2.normalize(curr_hist, curr_hist)
+
+        hist_similarity = cv2.compareHist(
+            prev_hist,
+            curr_hist,
+            cv2.HISTCMP_CORREL
+        )
+
+        
+        if hist_similarity > hist_threshold:
+            frame_index += 1
+            continue
+
+        
+        diff = cv2.absdiff(prev_gray, gray)
+        _, thresh = cv2.threshold(diff, 40, 255, cv2.THRESH_BINARY)
+
+        changed_pixels = np.count_nonzero(thresh)
+        area_ratio = changed_pixels / total_pixels
+
+        
+        if area_ratio > min_area_ratio:
+            frame_path = os.path.join(output_dir, f"frame_{saved_count:04d}.jpg")
+            cv2.imwrite(frame_path, frame)
+            frame_paths.append(frame_path)
+            saved_count += 1
+
+            prev_gray = gray
+            prev_hist = curr_hist
+
+        frame_index += 1
+
+    cap.release()
+
+    print(f"\nExtracted {saved_count} slide frames.")
+    return frame_paths
